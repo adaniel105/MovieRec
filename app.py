b2877c8f89a67e24262a0a0dad23cbd2a6b4cc51
Akinola Daniel <vantablack294@gmail.com>
2024-09-10 21:09:38 +0100

    Pipe movie list to view

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
@@ -13,14 +14,14 @@ app.mount("/static", StaticFiles(directory=str(static_folder)), name="static")
 templates = Jinja2Templates(directory=str(template_folder))
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 async def index(
-    request: Request, movie_name: str = "Toy", number_of_recommend: int = 7
+    request: Request, movie_name: str = "Spider-man", number_of_recommend: int = 7
 ):
     recommendations = MovieRecommender()
     movie_list = recommendations.create_rec(
         movie_name=movie_name, number_of_recommend=number_of_recommend
     )
     return templates.TemplateResponse(
-        "index.html", {"request": request, "recommendation": movie_list[3]}
+        "index.html", {"request": request, "movie_list": movie_list}
     )
