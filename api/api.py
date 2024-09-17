5dbf525e60c3a82558d74ddee3f68cc12cd2c74b
Akinola Daniel <vantablack294@gmail.com>
2024-09-17 21:51:54 +0100

    Oopsie

--- api/api.py
+++ api/api.py
@@ -1,16 +1,21 @@
+import os
 import requests
+from dotenv import load_dotenv
 
 # from data_preprocessing import movies
 
 
-title = "Money Train"
+load_dotenv()
+READ_ACCESS_TOKEN = os.getenv("READ_ACCESS_TOKEN")
+
+title = "Jumanji"
 # year = movies["year"].loc[movies["title"] == title].tolist()[0]
 
 url = f"https://api.themoviedb.org/3/search/tv?query={title}&include_adult=false&language=en-US&page=1"
 
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ZTA5MjYyZTZmYTIxZmFkODA0MDExZWNkNDI1MmNiOCIsIm5iZiI6MTcyNjUzODY5MS43NDYxMjgsInN1YiI6IjY2ZTM2NzA3MDAwMDAwMDAwMGI5NWMyNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.c3rnjOkBEML5gVC1fYyiJQvdwmtn5GDTT5amBZ6-iSo",
+    "Authorization": f"Bearer {READ_ACCESS_TOKEN}",
 }
 
 response = requests.get(url, headers=headers)
@@ -18,4 +23,4 @@ response = requests.get(url, headers=headers)
 response = response.json()
 
 
-response["results"][0]["overview"]
+print(response["results"][0]["overview"])
